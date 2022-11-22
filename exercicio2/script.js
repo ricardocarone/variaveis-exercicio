fcaa48bf741d3b66f12161326aed5f9ac9358a30
ricardocarone <ricardocaronefilho@gmail.com>
2022-11-21 22:13:38 -0300

    Exercício Variáveis 21/11

---
+++ exercicio2/script.js
@@ -0,0 +1,21 @@
+let a = 10;
+let b = 25;
+let aux = a
+
+
+
+
+
+// foi criado uma nova variável (aux) para auxiliar na troca dos numeros
+//cuidado com mais de um let*
+
+aux = a;
+a = b;
+b = aux;
+
+console.log("o novo valor de a é", a);
+console.log("O novo valor de b é", b);
+
+
+
+
